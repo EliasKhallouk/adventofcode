9dcea574f89b09f8e4d0385ca2fa441158270083
Elias Khallouk <eliaskhallouk@gmail.com>
2024-12-07 18:20:58 -0500

    Day One Part Two

---
+++ Day1/prog1-2.py
@@ -0,0 +1,36 @@
+import bisect
+
+# LECTURE DU FICHIER
+with open('/home/userdepinfo/PROJECT/adventofcode/Day1/input.txt', 'r') as file:
+    data = file.read()
+
+# REMPLISSAGE DES TABLEAUX
+data_left = []
+data_right = []
+i = 0
+j = 0
+while i < len(data):
+    if j % 2 == 0:
+        data_left.append(int(data[i:i+6]))
+        i += 8
+    else:
+        data_right.append(int(data[i:i+6]))
+        i += 6
+    j += 1
+
+'''
+print(data_left)
+print(data_right)
+'''
+
+# CALCUL DE LA SIMILARITE
+similaire = 0
+for i in data_left:
+    compteur = 0
+    for j in data_right:
+        if i == j:
+            compteur += 1
+    similaire += i * compteur
+
+# AFFICHAGE DE LA SIMILARITE
+print(similaire)
