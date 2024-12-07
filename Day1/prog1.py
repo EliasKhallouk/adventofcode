4c8092d8741ed8e4061f862dd69f9cdc94109622
Elias Khallouk <eliaskhallouk@gmail.com>
2024-12-07 17:54:43 -0500

    Day One

---
+++ Day1/prog1.py
@@ -0,0 +1,27 @@
+import bisect
+
+# LECTURE DU FICHIER
+with open('/home/userdepinfo/PROJECT/adventofcode/Day1/input.txt', 'r') as file:
+    data = file.read()
+
+# REMPLISSAGE DES TABLEAUX
+data_left = []
+data_right = []
+i = 0
+j = 0
+while i < len(data):
+    if j % 2 == 0:
+        bisect.insort(data_left, data[i:i+6])
+        i += 8
+    else:
+        bisect.insort(data_right, data[i:i+6])
+        i += 6
+    j += 1
+
+# CALCUL DE LA DIFFERENCE
+distance = 0
+for i in range (len(data_left)):
+    distance += abs(int(data_left[i]) - int(data_right[i]))
+
+# AFFICHAGE DE LA DISTANCE TOTAL
+print(distance)
