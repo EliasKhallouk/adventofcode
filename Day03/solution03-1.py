b65546cd7a2871e29a154cd9b6d861b66bf137f1
Elias Khallouk <eliaskhallouk@gmail.com>
2024-12-09 23:48:17 -0500

    comment

--- Day03/solution03-1.py
+++ Day03/solution03-1.py
@@ -1,16 +1,18 @@
+# LECTURE DU FICHIER
 with open('Day03/input.txt', 'r') as file:
     data = file.read()
 
-
+# CALCULE DU RESULTAT DE LA MULTIPLICATION
 data = data.split('mul(')
-compteur=0
+result=0
 for i in range(len(data)):
     try:
         data[i] = data[i].split(',')
         data[i][1] = data[i][1].split(')')
         if(0<int(data[i][0])<1000 and 0<int(data[i][1][0])<1000 and ' ' not in data[i][0] and ' ' not in data[i][1][0] and len(data[i][1])>1):
-            compteur+=int(data[i][0])*int(data[i][1][0])
+            result+=int(data[i][0])*int(data[i][1][0])
     except:
         pass
-    
-print(compteur) # --> 175615763
+
+# AFFICHAGE DU RESULTAT DE LA MULTIPLICATION
+print(result) # --> 175615763
